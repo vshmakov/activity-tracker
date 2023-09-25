422508f3595faafa7a97248984d5c71f326664df
VadimShmakov <shmakovvadim@mail.ru>
2023-09-25 21:37:14 +0500

    Ignored ts errors

--- src/fs-helpers.ts
+++ src/fs-helpers.ts
@@ -1,19 +1,22 @@
 /**
  * Open a handle to an existing file on the local file system.
  */
-function getFileHandle(): Promise<FileSystemFileHandle> {
+export function getFileHandle(): Promise<FileSystemFileHandle> {
     // For Chrome 86 and later...
     if ('showOpenFilePicker' in window) {
+        // @ts-ignore
         return window.showOpenFilePicker().then((handles) => handles[0]);
     }
+
     // For Chrome 85 and earlier...
+    // @ts-ignore
     return window.chooseFileSystemEntries();
 }
 
 /**
  * Create a handle to a new (text) file on the local file system.
  */
-function getNewFileHandle(): Promise<FileSystemFileHandle> {
+export function getNewFileHandle(): Promise<FileSystemFileHandle> {
     // For Chrome 86 and later...
     if ('showSaveFilePicker' in window) {
         const options = {
@@ -22,8 +25,11 @@ function getNewFileHandle(): Promise<FileSystemFileHandle> {
                 accept: {'text/plain': ['.txt']},
             }],
         };
+
+        // @ts-ignore
         return window.showSaveFilePicker(options);
     }
+
     // For Chrome 85 and earlier...
     const options = {
         type: 'save-file',
@@ -33,13 +39,15 @@ function getNewFileHandle(): Promise<FileSystemFileHandle> {
             mimeTypes: ['text/plain'],
         }],
     };
+
+// @ts-ignore
     return window.chooseFileSystemEntries(options);
 }
 
 /**
  * Reads the raw text from a file.
  */
-function readFile(file: File): Promise<string> {
+export function readFile(file: File): Promise<string> {
     // If the new .text() reader is available, use it.
     if (file.text) {
         return file.text();
@@ -52,10 +60,11 @@ function readFile(file: File): Promise<string> {
 /**
  * Reads the raw text from a file.
  */
-function _readFileLegacy(file: File): Promise<string> {
+export function _readFileLegacy(file: File): Promise<string> {
     return new Promise((resolve) => {
         const reader = new FileReader();
         reader.addEventListener('loadend', (e) => {
+            // @ts-ignore
             const text = e.srcElement.result;
             resolve(text);
         });
@@ -66,10 +75,12 @@ function _readFileLegacy(file: File): Promise<string> {
 /**
  * Writes the contents to disk.
  */
-async function writeFile(fileHandle: FileSystemFileHandle, contents: string): void {
+export async function writeFile(fileHandle: FileSystemFileHandle, contents: string): Promise<void> {
     // Support for Chrome 82 and earlier.
+    // @ts-ignore
     if (fileHandle.createWriter) {
         // Create a writer (request permission if necessary).
+        // @ts-ignore
         const writer = await fileHandle.createWriter();
         // Write the full length of the contents
         await writer.write(0, contents);
@@ -80,6 +91,7 @@ async function writeFile(fileHandle: FileSystemFileHandle, contents: string): vo
 
     // For Chrome 83 and later.
     // Create a FileSystemWritableFileStream to write to.
+    // @ts-ignore
     const writable = await fileHandle.createWritable();
     // Write the contents of the file to the stream.
     await writable.write(contents);
@@ -91,18 +103,22 @@ async function writeFile(fileHandle: FileSystemFileHandle, contents: string): vo
  * Verify the user has granted permission to read or write to the file, if
  * permission hasn't been granted, request permission.
  */
-async function verifyPermission(fileHandle: FileSystemFileHandle, withWrite: boolean): boolean {
+export async function verifyPermission(fileHandle: FileSystemFileHandle, withWrite: boolean): Promise<boolean> {
     const options = {};
     if (withWrite) {
+        // @ts-ignore
         options.writable = true;
         // For Chrome 86 and later...
+        // @ts-ignore
         options.mode = 'readwrite';
     }
     // Check if we already have permission, if so, return true.
+    // @ts-ignore
     if (await fileHandle.queryPermission(options) === 'granted') {
         return true;
     }
     // Request permission to the file, if the user grants permission, return true.
+    // @ts-ignore
     if (await fileHandle.requestPermission(options) === 'granted') {
         return true;
     }
